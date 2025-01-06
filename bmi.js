177e4766a1548247f056c3a6f6caa47d2afbe34b
Lukáš S. <stareclukas25@gmail.com>
2025-01-06 20:31:36 +0100

    Initial commit

---
+++ bmi.js
@@ -0,0 +1,8 @@
+mass = 75
+height = 175
+
+let bmi;
+
+bmi = mass / (height ** 2)
+
+console.log(bmi)
---
+++ temperature.js
@@ -0,0 +1,5 @@
+let fahrenreit = 30;
+
+celsius = (fahrenreit - 32)/1.8
+
+console.log(celsius);
